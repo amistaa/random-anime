a1d2f0be1f3ab37104c81c12423308c84f4c2b43
Amista <62178839+amistaa@users.noreply.github.com>
2020-03-31 23:56:43 +0700

    Update core.js

--- core/core.js
+++ core/core.js
@@ -1,14 +1,15 @@
 const databaseAnime = require('../database/anime.json')
+const databaseNSFW = require('../database/nsfw.json')
 
 function animeRandom() {
   return {
     anime: () => databaseAnime[mathRandom(databaseAnime.length)],
-    nsfw: () => databaseAnime[mathRandom(databaseAnime.length)]
+    nsfw: () => databaseNSFW[mathRandom(databaseNSFW.length)]
   }
 }
 
 function mathRandom(max) {
-    return Math.floor(Math.random() * max)
+    return ~~(Math.random() * max)
 }
 
 module.exports = animeRandom()
