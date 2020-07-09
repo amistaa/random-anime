2dda0005a91fa33c166ed07115aef0dc0f028982
amistaa <mystoganspace@gmail.com>
2020-07-09 18:56:56 +0700

    Update discord-example.js

--- example/discord-example.js
+++ example/discord-example.js
@@ -1,16 +1,16 @@
-const randomanime = require("random-anime");
+const randomAnime = require("random-anime");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", async message => {
   /* Plain Text */
   if (message.content === "plain") {
-    const anime = randomanime.anime();
+    const anime = randomAnime.anime();
     message.channel.send(anime);
   }
   /* Embed Image (D.JS Version 12) */
   if (message.content === "embed") {
-    const anime = randomanime.anime();
+    const anime = randomAnime.anime();
     const embed = new Discord.MessageEmbed().setImage(anime);
     message.channel.send(embed);
   }
