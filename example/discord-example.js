8198765057441a7235d2d6a3827f674345ca811a
Amista <62178839+amistaa@users.noreply.github.com>
2020-07-09 19:38:42 +0700

    Update discord-example.js

--- example/discord-example.js
+++ example/discord-example.js
@@ -3,12 +3,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", async message => {
-  /* Plain Text */
   if (message.content === "plain") {
     const anime = randomAnime.anime();
     message.channel.send(anime);
   }
-  /* Embed Image (D.JS Version 12) */
   if (message.content === "embed") {
     const anime = randomAnime.anime();
     const embed = new Discord.MessageEmbed().setImage(anime);
